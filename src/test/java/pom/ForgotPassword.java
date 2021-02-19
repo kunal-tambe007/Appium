10b80b380e94fae10c895b882c312be87450b753
Kunal Tambe <kunal_tambe2000@yahoo.co.in>
2021-02-19 22:09:48 +0530

    "This is forgot password" changes

---
+++ src/test/java/pom/ForgotPassword.java
@@ -0,0 +1,22 @@
+package pom;
+
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+public class ForgotPassword {
+
+	AndroidDriver<MobileElement> driver;
+
+	public ForgotPassword() {
+
+	}
+
+	public ForgotPassword(AndroidDriver<MobileElement> driver) {
+		this.driver = driver;
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+	}
+
+}
